8282363e37a9191e530aa54903c87fadf0c4a79c
RikoBato <s20c1101jd@s.chibakoudai.jp>
2021-07-13 09:25:27 +0900

    7/13

---
+++ train5/build/hellolib/CMakeFiles/uf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uf.dir/src/hellolib.cpp.o"
+  "libuf.pdb"
+  "libuf.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
